3c6da8cc048708ed74bc427597f92e057837141a
MacBookProBall <stoneofunicorn@gmail.com>
2019-09-02 12:30:44 +0700

    [test-1] add extension change UI

---
+++ proCheckPrimeNumber/Extension/Button.swift
@@ -0,0 +1,26 @@
+//
+//  Button.swift
+//  proCheckPrimeNumber
+//
+//  Created by Mac User on 1/9/2562 BE.
+//  Copyright © 2562 Mac User. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func setBtnSubmitOff() {
+        self.layer.cornerRadius = 8
+        self.backgroundColor = UIColor.lightGray
+        self.setTitleColor(UIColor.gray, for: .normal)
+        self.isEnabled = false
+    }
+    
+    func setBtnSubMitON() {
+        self.layer.cornerRadius = 8
+        self.backgroundColor = UIColor.gray
+        self.setTitleColor(UIColor.black, for: .normal)
+        self.isEnabled = true
+    }
+}

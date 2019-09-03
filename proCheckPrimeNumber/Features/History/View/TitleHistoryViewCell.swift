129cb833211858a3c3dd737d9b6cba700a87f170
MacBookProBall <stoneofunicorn@gmail.com>
2019-09-03 15:11:58 +0700

    [test-2] create layout history and code

---
+++ proCheckPrimeNumber/Features/History/View/TitleHistoryViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TitleHistoryViewCell.swift
+//  proCheckPrimeNumber
+//
+//  Created by Mac User on 2/9/2562 BE.
+//  Copyright © 2562 Mac User. All rights reserved.
+//
+
+import Foundation
